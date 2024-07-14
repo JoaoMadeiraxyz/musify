1bf3528f647cc0d65bc268ce28e99d9d7227cb92
JoaoMadeiraxyz <jaodisc@gmail.com>
2024-07-14 16:07:32 -0300

    add phosphoricons dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
 import { Button } from "./components/Button";
+
+import { AirplaneTakeoff } from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-5">
       <Button />
+      <AirplaneTakeoff size={32} weight="duotone" />
     </main>
   );
 }
