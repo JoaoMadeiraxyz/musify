d3eeaf5502c66a1e1faeb3eddfa6b65190554f67
ClaudiaC137 <claudia.oliveirazix@gmail.com>
2024-11-26 08:20:26 -0300

    Crud de Carrinho de compras

---
+++ src/services/firebase/cart/Update.ts
@@ -0,0 +1,22 @@
+import { db } from "../../../../../config";
+import { doc, updateDoc } from "firebase/firestore";
+const CART_COLLECTION = "cart";
+
+type UpdateItemProps = {
+  item_id: string;
+  data: Partial<{
+    name: string;
+    price: number;
+  }>;
+};
+
+export async function updateCartItem({ item_id, data }: UpdateItemProps) {
+  try {
+    const itemRef = doc(db, CART_COLLECTION, item_id);
+    await updateDoc(itemRef, data);
+    console.log("Cart item updated:", item_id);
+  } catch (error) {
+    console.error("Error updating cart item:", error);
+    throw new Error("Failed to update cart item.");
+  }
+}
---
+++ src/services/firebase/cart/create.ts
@@ -0,0 +1,23 @@
+import { db } from "../../../../../config";
+import { collection, addDoc } from "firebase/firestore";
+
+const CART_COLLECTION = "cart";
+
+type AddItemProps = {
+  id: string;
+  type: "album" | "music" | "artist";
+  name: string;
+  price: number;
+  user_id: string;
+};
+
+export async function addItemToCart(item: AddItemProps) {
+  try {
+    const cartCollectionRef = collection(db, CART_COLLECTION);
+    await addDoc(cartCollectionRef, item);
+    console.log("Item added to cart:", item);
+  } catch (error) {
+    console.error("Error adding item to cart:", error);
+    throw new Error("Failed to add item to cart."); 
+  }
+}
